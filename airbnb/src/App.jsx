62bedb84ea4505c4fe387fc2b4e65b7f0303e7da
Jean <acalto2@gmail.com>
2024-07-13 23:24:03 +0200

    props

--- airbnb/src/App.jsx
+++ airbnb/src/App.jsx
@@ -1,21 +1,22 @@
-
-import './App.css'
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import React from "react"
-import Card from './components/Card'
-
-/*
-Challenge: Build the Navbar component.
-Check the Figma file for the design specifics.
-*/
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import React from "react";
+import Card from "./components/Card";
 
 export default function App() {
-    return (
-      <>
-        {/* <Navbar /> */}
-        {/* <Hero /> */}
-        <Card />
-      </>
-    )
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Card
+        img="image1.png"
+        rating="5.0"
+        reviewCount="6"
+        country="USA"
+        title="Life Lessons with Katie Zaferes"
+        price="136"
+      />
+    </>
+  );
 }
--- airbnb/src/components/Card.jsx
+++ airbnb/src/components/Card.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 
-export default function Card (){
-    return(
-        <div className="card">
-            <img className="card--image" src="src/assets/image1.png" alt="" />
-            <div className="card--stats">
-                <img className="star" src="src/assets/star.png" alt="" />
-                <span>5.0</span>
-                <span className="gray">(6) • </span>
-                <span className="gray">USA</span>
-            </div>
-            <p>Life lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136</span> / person</p>
-        </div>
-    )
+export default function Card(props) {
+  // console.log(props);
+  return (
+    <div className="card">
+      {/* <img className="card--image" src="src/assets/{props.img}" alt="" /> */}
+      <img src={`src/assets/${props.img}`} className="card--image" />
+      <div className="card--stats">
+        <img className="star" src="src/assets/star.png" alt="" />
+        <span>{props.rating}</span>
+        <span className="gray">({props.reviewCount}) • </span>
+        <span className="gray">{props.country}</span>
+      </div>
+      <p>{props.title}</p>
+      <p>
+        <span className="bold">From ${props.price}</span> / person
+      </p>
+    </div>
+  );
 }
