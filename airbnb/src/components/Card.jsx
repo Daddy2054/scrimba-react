9dea81e7281e313b3eea7b6ef9be4a41b13e91a4
Jean <acalto2@gmail.com>
2024-07-14 18:39:58 +0200

    Sold Out Badge

--- airbnb/src/components/Card.jsx
+++ airbnb/src/components/Card.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
 export default function Card(props) {
-  // console.log(props);
+  let badgeText
+  if (props.openSpots === 0) {
+      badgeText = "SOLD OUT"
+  } else if (props.location === "Online") {
+      badgeText = "ONLINE"
+  }
   return (
     <div className="card">
+      {badgeText && <div className="card--badge">{badgeText}</div>}
       <img src={`src/images/${props.img}`} className="card--image" />
       <div className="card--stats">
         <img className="star" src="src/assets/star.png" alt="" />
