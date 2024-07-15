5ac74c679b3290bf58d44986cbcf044a94d48712
Jean <acalto2@gmail.com>
2024-07-15 19:01:47 +0200

    pass spreading object

--- airbnb/src/components/Card.jsx
+++ airbnb/src/components/Card.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 export default function Card(props) {
+  console.log(props)
   let badgeText
   if (props.openSpots === 0) {
       badgeText = "SOLD OUT"
@@ -10,11 +11,11 @@ export default function Card(props) {
   return (
     <div className="card">
       {badgeText && <div className="card--badge">{badgeText}</div>}
-      <img src={`src/images/${props.img}`} className="card--image" />
+      <img src={`src/images/${props.coverImg}`} className="card--image" />
       <div className="card--stats">
         <img className="star" src="src/assets/star.png" alt="" />
-        <span>{props.rating}</span>
-        <span className="gray">({props.reviewCount}) • </span>
+        <span>{props.stats.rating}</span>
+        <span className="gray">({props.stats.reviewCount}) • </span>
         <span className="gray">{props.location}</span>
       </div>
       <p>{props.title}</p>
