3a6806e5fc4f0fa67db4cad27b88ce0e29f44765
Jean <acalto2@gmail.com>
2024-07-17 11:33:16 +0200

    state object

--- meme-generator/src/components/Form.jsx
+++ meme-generator/src/components/Form.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import memesData from "../../memesData";
 export default function Form() {
-const [memeImage, setMemeImage] = React.useState("");
+  const[meme,setMeme]=React.useState({
+    topText:"",
+    bottomText:"",
+    randomImage:"http://i.imgflip.com/1bij.jpg"
+  });
+  const[allMemes,setAllMemes]=React.useState(memesData);
+// const [memeImage, setMemeImage] = React.useState("");
+
   function getMemeImage() {
-    const meme = memesData.data.memes[Math.floor(Math.random() * memesData.data.memes.length)];
-    const {url} = meme;
-    console.log(url);
-    setMemeImage(url);
+    const memesArray = allMemes.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    setMeme(prevMeme => ({
+      ...prevMeme,
+      randomImage: url
+    }));
+    
   }
+//     const meme = memesData.data.memes[Math.floor(Math.random() * memesData.data.memes.length)];
+//     const {url} = meme;
+//     console.log(url);
+//     setMemeImage(url);
+//   }
   return (
     <>
     <div className="form">
@@ -28,7 +44,7 @@ const [memeImage, setMemeImage] = React.useState("");
         onClick={getMemeImage}
         />
     </div>
-    <img className="meme--image" src={memeImage} />
+    <img className="meme--image" src={meme.randomImage} />
         </>
   );
 }
