f2ad2ecb011b8a23eb0b92f545fd8f86a0c7395f
Jean <acalto2@gmail.com>
2024-07-23 18:12:14 +0200

    forms radio

--- forms/src/App.jsx
+++ forms/src/App.jsx
@@ -1,73 +1,105 @@
-/* eslint-disable no-unused-vars */
-import { useState } from "react";
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import "./App.css";
-// import React from "react"
+import React from "react"
 
-function App() {
-  const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    comments: "",
-    isFriendly: true,
-  });
-
-  // console.log(formData);
-
-  function handleChange(event) {
-    const {name, value,type, checked} = event.target
-    setFormData(prevFormData => {
-        return {
-            ...prevFormData,
-            [name]: type === "checkbox" ? checked : value
-          }
-    })
+export default function App() {
+    const [formData, setFormData] = React.useState(
+        {
+            firstName: "", 
+            lastName: "", 
+            email: "", 
+            comments: "", 
+            isFriendly: true,
+            employment: ""
+        }
+    )
+    // console.log(formData.employment)
+    
+    function handleChange(event) {
+        const {name, value, type, checked} = event.target
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [name]: type === "checkbox" ? checked : value
+            }
+        })
+    }
+    
+    return (
+        <form>
+            <input
+                type="text"
+                placeholder="First Name"
+                onChange={handleChange}
+                name="firstName"
+                value={formData.firstName}
+            />
+            <input
+                type="text"
+                placeholder="Last Name"
+                onChange={handleChange}
+                name="lastName"
+                value={formData.lastName}
+            />
+            <input
+                type="email"
+                placeholder="Email"
+                onChange={handleChange}
+                name="email"
+                value={formData.email}
+            />
+            <textarea 
+                value={formData.comments}
+                placeholder="Comments"
+                onChange={handleChange}
+                name="comments"
+            />
+            <input 
+                type="checkbox" 
+                id="isFriendly" 
+                checked={formData.isFriendly}
+                onChange={handleChange}
+                name="isFriendly"
+            />
+            <label htmlFor="isFriendly">Are you friendly?</label>
+            <br />
+            <br />
+            
+            <fieldset>
+                <legend>Current employment status</legend>
+                
+                <input 
+                    type="radio"
+                    id="unemployed"
+                    name="employment"
+                    value="unemployed"
+                    checked={formData.employment === "unemployed"}
+                    onChange={handleChange}
+                />
+                <label htmlFor="unemployed">Unemployed</label>
+                <br />
+                
+                <input 
+                    type="radio"
+                    id="part-time"
+                    name="employment"
+                    value="part-time"
+                    checked={formData.employment === "part-time"}
+                    onChange={handleChange}
+                />
+                <label htmlFor="part-time">Part-time</label>
+                <br />
+                
+                <input 
+                    type="radio"
+                    id="full-time"
+                    name="employment"
+                    value="full-time"
+                    checked={formData.employment === "full-time"}
+                    onChange={handleChange}
+                />
+                <label htmlFor="full-time">Full-time</label>
+                <br />
+                
+            </fieldset>
+        </form>
+    )
 }
-
-  return (
-    <form className="form">
-      <input
-        type="text"
-        placeholder="First Name"
-        onChange={handleChange}
-        name="firstName"
-        value={formData.firstName} //controlled input
-      />
-      <input
-        type="text"
-        placeholder="Last Name"
-        onChange={handleChange}
-        name="lastName"
-        value={formData.lastName}
-      />
-      <input
-        type="email"
-        placeholder="Email"
-        onChange={handleChange}
-        name="email"
-        value={formData.email}
-      />
-      <textarea
-        type="textarea"
-        placeholder="Comments"
-        onChange={handleChange}
-        name="comments"
-        value={formData.comments}
-      />
-      <input
-        type="checkbox"
-        id="isFriendly"
-        checked={formData.isFriendly}
-        onChange={handleChange}
-        name="isFriendly"
-
-      />
-      <label htmlFor="isFriendly">Are you friendly?</label>
-      <br />
-    </form>
-  );
-}
-
-export default App;
