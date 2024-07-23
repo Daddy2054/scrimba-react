00a380f05949bf491f232e9e67a2e89ad4741fc3
Jean <acalto2@gmail.com>
2024-07-23 21:35:55 +0200

    useEffect for fetch

--- api/src/App.jsx
+++ api/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+/* eslint-disable no-unused-vars */
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import React from "react"
 
+export default function App() {
+    const [starWarsData, setStarWarsData] = React.useState({})
+    const [count, setCount] = React.useState(0)
+
+    React.useEffect(function() {
+      console.log("Effect ran")
+      fetch("https://swapi.dev/api/people/1")
+          .then(res => res.json())
+          .then(data => setStarWarsData(data))
+  }, [])
+  
   return (
-    <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+          <h2>The count is {count}</h2>
+          <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
-export default App
+
