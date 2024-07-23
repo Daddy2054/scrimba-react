eaacfb70dc62fbe20f0a240935d47511d66b1904
Jean <acalto2@gmail.com>
2024-07-23 21:49:44 +0200

    useEffect for fetch 2

--- api/src/App.jsx
+++ api/src/App.jsx
@@ -7,19 +7,20 @@ export default function App() {
     const [starWarsData, setStarWarsData] = React.useState({})
     const [count, setCount] = React.useState(0)
 
+    
     React.useEffect(function() {
       console.log("Effect ran")
-      fetch("https://swapi.dev/api/people/1")
+      fetch(`https://swapi.dev/api/people/${count}`)
           .then(res => res.json())
           .then(data => setStarWarsData(data))
-  }, [])
+  }, [count])
   
   return (
-      <div>
-          <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
-          <h2>The count is {count}</h2>
-          <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
-      </div>
+    <div>
+    <h2>The count is {count}</h2>
+    <button onClick={() => setCount(prevCount => prevCount + 1)}>Get Next Character</button>
+    <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+</div>
   )
 }
 
